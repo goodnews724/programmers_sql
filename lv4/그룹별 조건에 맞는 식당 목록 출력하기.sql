d0af1a53005cf7b5c8b75b8aefab29ec338125f9
goodnews724 <qazkhj@gmail.com>
2025-08-27 10:22:49 +0900

    4-2 그룹별 조건에 맞는 식당 파티션으로도 풀었음

--- lv4/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ lv4/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -25,27 +25,58 @@
 --     RR.REVIEW_DATE ASC, RR.REVIEW_TEXT ASC;
 
 -- 가장 리뷰를 많이 작성한 회원이 여러 명일 경우 모두 포함
+-- SELECT 
+--     MP.MEMBER_NAME,
+--     RR.REVIEW_TEXT,
+--     LEFT(RR.REVIEW_DATE, 10) as REVIEW_DATE
+-- FROM 
+--     MEMBER_PROFILE MP
+-- JOIN 
+--     REST_REVIEW RR ON MP.MEMBER_ID = RR.MEMBER_ID
+-- WHERE 
+--     MP.MEMBER_ID IN (
+--         SELECT MEMBER_ID
+--         FROM REST_REVIEW 
+--         GROUP BY MEMBER_ID
+--         HAVING COUNT(*) = (
+--             SELECT MAX(cnt) 
+--             FROM (
+--                 SELECT COUNT(*) as cnt 
+--                 FROM REST_REVIEW 
+--                 GROUP BY MEMBER_ID
+--             ) review_count_table
+--         )
+--     )
+-- ORDER BY 
+--     RR.REVIEW_DATE ASC, RR.REVIEW_TEXT ASC;
+
+
+-- 파티션 DENSE_RANK() 사용 -> 동시 1등인 회원 모두 포함
+WITH review_stats AS (
+    SELECT 
+        MP.MEMBER_ID,
+        MP.MEMBER_NAME,
+        RR.REVIEW_TEXT,
+        RR.REVIEW_DATE,
+        COUNT(*) OVER (PARTITION BY MP.MEMBER_ID) as review_count
+    FROM MEMBER_PROFILE MP
+    JOIN REST_REVIEW RR ON MP.MEMBER_ID = RR.MEMBER_ID
+),
+ranked_reviews AS (
+    SELECT 
+        MEMBER_NAME,
+        REVIEW_TEXT,
+        REVIEW_DATE,
+        DENSE_RANK() OVER (ORDER BY review_count DESC) as rnk
+    FROM review_stats
+)
 SELECT 
-    MP.MEMBER_NAME,
-    RR.REVIEW_TEXT,
-    LEFT(RR.REVIEW_DATE, 10) as REVIEW_DATE
+    MEMBER_NAME,
+    REVIEW_TEXT,
+    LEFT(REVIEW_DATE, 10) as REVIEW_DATE
 FROM 
-    MEMBER_PROFILE MP
-JOIN 
-    REST_REVIEW RR ON MP.MEMBER_ID = RR.MEMBER_ID
-WHERE 
-    MP.MEMBER_ID IN (
-        SELECT MEMBER_ID
-        FROM REST_REVIEW 
-        GROUP BY MEMBER_ID
-        HAVING COUNT(*) = (
-            SELECT MAX(cnt) 
-            FROM (
-                SELECT COUNT(*) as cnt 
-                FROM REST_REVIEW 
-                GROUP BY MEMBER_ID
-            ) review_count_table
-        )
-    )
+    ranked_reviews
+WHERE
+    rnk = 1
 ORDER BY 
-    RR.REVIEW_DATE ASC, RR.REVIEW_TEXT ASC;
+    REVIEW_DATE ASC, REVIEW_TEXT ASC;
